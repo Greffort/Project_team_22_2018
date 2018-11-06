c684306d1d896d88742cf63c73af6b3882bb7126
Aleks <Aleks_Kravchina@outlook.com>
2018-11-06 19:55:25 +0400

    Подключены PMD, spotbugs. Добавлен .gitignore

--- src/main/java/helloWorld/HelloWorld.java
+++ src/main/java/helloWorld/HelloWorld.java
@@ -1,7 +1,12 @@
-package helloWorld;
+package helloworld;
 
+/**
+ * @author HelloWorld
+ */
 public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World!!!");
-    }
+
+		public static void main(final String[] args) {
+			System.out.println("Hello World!!!");
+		}
+
 }
