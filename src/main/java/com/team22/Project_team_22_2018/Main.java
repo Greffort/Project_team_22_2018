95cade88cb2aa850d529bb342c7b6dc6b0f751c1
Aleks <Aleks_Kravchina@outlook.com>
2018-11-13 12:25:07 +0400

    1. Connect plugin JavaFX. 2. Changed project structure. 3. Create Login Form (empty).

--- src/main/java/com/team22/Project_team_22_2018/Main.java
+++ src/main/java/com/team22/Project_team_22_2018/Main.java
@@ -1,10 +1,8 @@
 package com.team22.Project_team_22_2018;
 
 import com.futur.common.helpers.resources.FXMLHelper;
-import com.futur.common.log.LogInitializer;
 import com.team22.Project_team_22_2018.service.ResourceService;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -16,8 +14,6 @@ import org.jetbrains.annotations.NotNull;
  */
 public class Main extends Application {
     @NotNull
-    private static final String LOG_PROPERTY_NAME = "ISLogbackPath";
-    @NotNull
     private static final String APPLICATION_NAME = "team_22_project";
 
 
@@ -25,30 +21,16 @@ public class Main extends Application {
         launch(args);
     }
 
-//    @Override
-//    public void start(Stage primaryStage) throws Exception {
-//        Parent root = FXMLLoader.load(getClass().getResource("resources/loginForm.fxml"));
-//        primaryStage.setTitle("ViewTask");
-//        primaryStage.setScene(new Scene(root, 600, 402));
-//        primaryStage.show();
-//    }
-//}
-
-
     private static final int APPLICATION_WIDTH = 600;
-    private static final int APPLICATION_HEIGHT = 800;
-
-
+    private static final int APPLICATION_HEIGHT = 400;
 
     @Override
     public void start(@NotNull final Stage primaryStage) throws Exception {
-
-        LogInitializer.initLogFileName(APPLICATION_NAME, LOG_PROPERTY_NAME);
-
         @NotNull final Parent root = FXMLHelper.loadNode(ResourceService.START_LAYOUT_FXML);
         @NotNull final Scene scene = new Scene(root, APPLICATION_WIDTH, APPLICATION_HEIGHT);
 
         primaryStage.setTitle(APPLICATION_NAME);
         primaryStage.setScene(scene);
         primaryStage.show();
-    }}
+    }
+}
--- src/main/java/com/team22/Project_team_22_2018/service/ResourceService.java
+++ src/main/java/com/team22/Project_team_22_2018/service/ResourceService.java
@@ -3,6 +3,7 @@ package com.team22.Project_team_22_2018.service;
 import com.futur.common.helpers.StringHelper;
 import com.futur.common.helpers.resources.ResourcesHelper;
 import org.jetbrains.annotations.NotNull;
+
 import java.net.URL;
 
 public final class ResourceService {
