f905d67543b7db6c6e6d50927c9596ea52da34ff
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2018-11-15 23:01:42 +0400

    Add files via upload

---
+++ src/main/java/com/team22/Project_team_22_2018/controllers/ControllerCreateTaskForm.java
@@ -0,0 +1,15 @@
+package com.team22.Project_team_22_2018.controllers;
+
+import javafx.application.Platform;
+import javafx.stage.Stage;
+
+import java.awt.*;
+
+public class ControllerCreateTaskForm {
+
+    Button button;
+
+    public void closeWindow(){
+        Platform.exit();
+    }
+}
